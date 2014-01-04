76d6ca3674893d1ca6d13f890f9788539270b425
Nathan Totten <nathan@ntotten.com>
2014-01-04 12:05:00 -0800

    JS updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,23 +10,3 @@ var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-17658402-1']);
 _gaq.push(['_trackPageview']);
 setTimeout("_gaq.push(['_trackEvent', '15_seconds', 'read'])",15000);
-
-// Load External Scripts
-function loadScripts() {
-  var s = document.getElementsByTagName('script')[0];
-  var loadScript = function(src) {
-    var js = document.createElement('script');
-    js.type = 'text/javascript';
-    js.src = src;
-    js.async = true;
-    s.parentNode.insertBefore(js, s);
-  };
-  loadScript("http://connect.facebook.net/en_US/all.js#xfbml=1&appId=355508947902562");
-  loadScript("http://platform.twitter.com/widgets.js");
-  loadScript("http://www.google-analytics.com/ga.js");
-}
-if (window.addEventListener)
-window.addEventListener("load", loadScripts, false);
-else if (window.attachEvent)
-window.attachEvent("onload", loadScripts);
-else window.onload = loadScripts;
