a70785ad44c03fe70bb942ef079023c19898a8af
Nathan Totten <ntotten@microsoft.com>
2014-05-20 14:43:37 -0700

    Updated scripts

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,9 +4,3 @@ for (var i = elements.length - 1; i >= 0; i--) {
   elements[i].setAttribute('class', 'prettyprint');
 };
 prettyPrint();
-
-// Google Analytics
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-17658402-1']);
-_gaq.push(['_trackPageview']);
-setTimeout("_gaq.push(['_trackEvent', '15_seconds', 'read'])",15000);
