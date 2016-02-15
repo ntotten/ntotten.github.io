dfc5df132832c42e5f6b4daf1ca2598754dac0ae
Nathan Totten <nathan@ntotten.com>
2016-02-14 16:59:19 -0800

    Highlight.js

--- assets/js/app.js
+++
@@ -1,6 +0,0 @@
-// Prettyprint
-var elements = document.getElementsByTagName('code');
-for (var i = elements.length - 1; i >= 0; i--) {
-  elements[i].setAttribute('class', 'prettyprint');
-};
-prettyPrint();
